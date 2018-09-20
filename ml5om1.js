1fc33ec889fb282edded378e8befd2c1476ead55
ml5om <43390029+ml5om@users.noreply.github.com>
2018-09-20 20:03:35 +0300

    Update ml5om1.js

--- ml5om1.js
+++ ml5om1.js
@@ -14,9 +14,68 @@ client.on('ready', () => {
   console.log('---------------')
 });
 
+
+     function commandIs(str, msg){
+            return msg.content.toLowerCase().startsWith('+' + str);
+        }
+
+        function pluck(array) {
+            return array.map(function(item) { return item['name']; });
+        }
+
+        function hasRole(mem, role) {
+            if(pluck(mem.roles).includes(role)){
+                return true;
+            } else {
+                return false;
+            }
+
+          }
+
+
+
+
+
+        var servers = {};
+
+
+
+
+
+
+        var q1 = "+quran 1"
+
+        var q2 = "+quran 2"
+
+        var q3 = "+quran 3"
+
+        var q4 = "+quran 4"
+
+
+
+
+
+        function play(connection, message) {
+            var server = servers[message.guild.id];
+
+            server.dispatcher = connection.playStream(yt(server.queue[0], { fliter: "audionly" }));
+
+            server.queue.shift();
+
+            server.dispatcher.on("end", function() {
+                if (server.queue[0]) play(connection, message);
+                else connection.disconnect();
+            });
+        }
+
+
+
+        //sowar
+
+
         client.on("message", message => {
 
-                                if (message.content === +q1 ) {
+                                if (message.content === q1 ) {
                           message.react('🔊')
             const voiceChannel = message.member.voiceChannel;
             if (!voiceChannel) {
@@ -41,6 +100,33 @@ client.on('ready', () => {
                 const dispatcher = connnection.playStream(stream);
               });
           }
+
+                                    if (message.content === q3 ) {
+                          message.react('🔊')
+            const voiceChannel = message.member.voiceChannel;
+            if (!voiceChannel) {
+              return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
+            }
+            voiceChannel.join()
+              .then(connnection => {
+                let stream = yt('https://www.youtube.com/watch?v=4JvY-MccxNk', {audioonly: true});
+                const dispatcher = connnection.playStream(stream);
+              });
+          }
+
+                                      if (message.content === q4 ) {
+                          message.react('🔊')
+            const voiceChannel = message.member.voiceChannel;
+            if (!voiceChannel) {
+              return message.reply(`يرجى أن تكون في قناة صوتيه أولا!`);
+            }
+            voiceChannel.join()
+              .then(connnection => {
+                let stream = yt('https://www.youtube.com/watch?v=Ktync4j_nmA', {audioonly: true});
+                const dispatcher = connnection.playStream(stream);
+                });
+          }
+
 
 
             //outher_cummon
